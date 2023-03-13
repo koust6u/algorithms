c34a01531f886437ad8e3f9375d834ea7c061c92
김민종 <godminjong@naver.com>
2023-03-13 17:16:43 +0900

    [Gold IV] Title: 전화번호 목록, Time: 116 ms, Memory: 2684 KB -BaekjoonHub

---
+++ 백준/Gold/5052. 전화번호 목록/전화번호 목록.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main(){
+    int test;
+    cin >> test;
+    vector<string>answer;
+
+    for(int t = 0; t < test; t++) {
+        bool flag = false;
+        int number;
+        cin >> number;
+        vector<string> v(number);
+        for (int i = 0; i < number; i++) {
+            cin >> v[i];
+        }
+        sort(v.begin(),v.end());
+        for(int i = 0 ; i < v.size()-1; i++){
+            if(v[i].length() > v[i+1].length()) continue;
+            if(v[i+1].substr(0, v[i].length())== v[i]) flag = true;
+        }
+        if(flag) answer.emplace_back("NO");
+        else answer.emplace_back("YES");
+    }
+    for(auto i: answer){
+        cout << i << "\n";
+    }
+}
