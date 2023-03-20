299e002788611541d43e26fd2062bac6842e4c02
김민종 <godminjong@naver.com>
2023-03-21 07:25:07 +0900

    [unrated] Title: 테이블 해시 함수, Time: 0.01 ms, Memory: 3.65 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/147354. 테이블 해시 함수/테이블 해시 함수.cpp
@@ -0,0 +1,28 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int global;
+bool compare(vector<int> a,vector<int> b){
+    if(a[global] == b[global]){
+        return a[0] > b[0];
+    }
+    return a[global] < b[global];
+}
+
+int solution(vector<vector<int>> data, int col, int row_begin, int row_end) {
+    int answer = 0;
+    global= col-1;
+    sort(data.begin(), data.end(), compare);
+    int xorSum = 0;
+    for(int i = row_begin-1 ; i < row_end; i++){
+        int mod = i+1;
+        int sum = 0;
+        for(int j= 0 ; j < data[0].size(); j++){
+            sum += (data[i][j]%mod);
+        }
+        xorSum = sum ^ xorSum;
+    }
+    return xorSum;
+}
